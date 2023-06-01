77a2c10a81238f999a36dde47c39005a90414183
Tosiek <antonizieciak@gmail.com>
2023-06-01 16:24:41 +0200

    home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,51 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using footty.Models;
+using Footty.Data;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
 
-namespace footty.Controllers;
+
+namespace footty.Controllers {
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly FoottyContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, FoottyContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        ViewData["team"] = "FC Barcelona";
-        return View();
+        var team = HttpContext.Session.GetString("favTeam");
+        var list = await _context.Match
+                                .Include(m => m.team)
+                                .Include(m => m.opponent)
+                                .ToListAsync();
+
+        list = list.Where(p => (p.team!.name == team || p.opponent!.name == team) && p.place=="Home")
+                    .OrderByDescending(p => p.date)
+                    .Take(5).ToList();
+
+        var stadiums = await _context.Stadium.Include(s => s.team).ToListAsync();
+        string longitude = stadiums.Where(p => p.team!.name == team).Select(p => p.longitude).First().ToString();
+        string latitude = stadiums.Where(p => p.team!.name == team).Select(p => p.latitude).First().ToString();
+
+        ViewData["fav_team"] = team;
+        ViewData["long"] = longitude;
+        ViewData["lat"] = latitude;
+        return View(list);
     }
 
     public IActionResult Privacy()
@@ -29,4 +58,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
 }
